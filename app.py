288a2eb1af971bd64ab4f14b93afb1933e3b261e
Dominik Haeusser <haeusser-dominik@web.de>
2020-11-29 15:45:06 +0100

    add load_png function

--- app.py
+++ app.py
@@ -7,6 +7,7 @@
 
 VERSION = "0.4"
 
+# Load modules
 try:
     import sys
     import random
@@ -15,6 +16,28 @@ try:
     import getopt
     from socket import *
     from pygame.locals import *
+    import pygame
 except ImportError as err:
     print("couldn't load module:", err)
     sys.exit(2)
+
+
+# Resource handling classes/functions
+def load_png(name):
+    """Load image and return image object"""
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+        if image.get_alpha() is None:
+            image = image.convert()
+        else:
+            image = image.convert_alpha()
+    except pygame.error as message:
+        print('cannot load image:', image)
+        raise (SystemExit, message)
+    return image, image.get_rect()
+
+# Game object classes
+# Any other game functions
+# Initialise the game
+# The main loop
